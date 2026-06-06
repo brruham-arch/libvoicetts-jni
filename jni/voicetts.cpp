b9c923b9cd10152b5b00a5ff1a687eb43a31e307
brruham <brruham@gmail.com>
2026-06-06 13:02:46 +0700

    fix: libart path untuk APEX Android

--- jni/voicetts.cpp
+++ jni/voicetts.cpp
@@ -352,8 +352,10 @@ EXPORT void OnModLoad() {
     LOGF("[TTS] OnModLoad start");
 
     // Ambil JavaVM dari proses game
-    void* hJavaVM = dlopen("libdvm.so", RTLD_NOW | RTLD_NOLOAD);
-    if (!hJavaVM) hJavaVM = dlopen("libart.so", RTLD_NOW | RTLD_NOLOAD);
+    void* hJavaVM = dlopen("libart.so", RTLD_NOW | RTLD_NOLOAD);
+    if (!hJavaVM) hJavaVM = dlopen("/apex/com.android.art/lib/libart.so", RTLD_NOW | RTLD_NOLOAD);
+    if (!hJavaVM) hJavaVM = dlopen("/apex/com.android.art/lib64/libart.so", RTLD_NOW | RTLD_NOLOAD);
+    if (!hJavaVM) hJavaVM = dlopen("libdvm.so", RTLD_NOW | RTLD_NOLOAD);
     if (!hJavaVM) { LOGF("[TTS] ERROR: libart/libdvm not found"); return; }
 
     typedef jint (*JNI_GetCreatedJavaVMs_t)(JavaVM**, jsize, jsize*);
